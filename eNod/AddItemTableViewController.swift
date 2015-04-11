4c5798ba8944541fb1b7a760eb3b57ec7a5d103b
Jonathan Prince <jonathan.prince@gmail.com>
2015-04-11 15:21:31 +0200

    create add item view controller

---
+++ eNod/AddItemTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  AddItemTableViewController.swift
+//  eNod
+//
+//  Created by Jonathan Prince on 11.04.15.
+//  Copyright (c) 2015 Jonathan Prince. All rights reserved.
+//
+
+import UIKit
+
+class AddItemTableViewController: UITableViewController {
+    
+    @IBAction func cancel(){
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    @IBAction func done(){
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+}
