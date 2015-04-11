ea17fffaabc4a8e9b6443f0e4ae345b7ea13af2c
Jonathan Prince <jonathan.prince@gmail.com>
2015-04-11 14:51:02 +0200

    create checklist item model, add dummy list items

---
+++ eNod/ChecklistItem.swift
@@ -0,0 +1,18 @@
+//
+//  ChecklistItem.swift
+//  eNod
+//
+//  Created by Jonathan Prince on 11.04.15.
+//  Copyright (c) 2015 Jonathan Prince. All rights reserved.
+//
+
+import Foundation
+
+class ChecklistItem {
+    var text = ""
+    var checked = false
+    
+    func toggleChecked() {
+        checked = !checked
+    }
+}
