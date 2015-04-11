c923c707ee3a604e97ed870030c8fbe1704a5f00
Jonathan Prince <jonathan.prince@gmail.com>
2015-04-11 16:18:27 +0200

    update add item view controller

--- eNod/AddItemTableViewController.swift
+++ eNod/AddItemTableViewController.swift
@@ -8,13 +8,37 @@
 
 import UIKit
 
-class AddItemTableViewController: UITableViewController {
+class AddItemTableViewController: UITableViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var doneBarButton: UIBarButtonItem!
     
     @IBAction func cancel(){
         dismissViewControllerAnimated(true, completion: nil)
     }
     
     @IBAction func done(){
+        println("Text: \(textField.text)")
         dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    override func tableView(tableView: UITableView, willSelectRowAtIndexPath indexPath: NSIndexPath) -> NSIndexPath? {
+        return nil
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        textField.becomeFirstResponder()
+    }
+    
+    func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
+        
+        let oldText: NSString = textField.text
+        let newText: NSString = oldText.stringByReplacingCharactersInRange(range, withString: string)
+
+        doneBarButton.enabled = (newText.length > 0)
+        
+        return true
+        
     }
 }
